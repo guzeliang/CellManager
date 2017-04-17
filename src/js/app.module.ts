4b328bc55fedb72d4bcbe6d87556c9ccb8029b6e
zhanglei <lei.zhang@sunscell.com>
2017-04-17 16:49:28 +0800

    merge

--- src/js/app.module.ts
+++ src/js/app.module.ts
@@ -6,11 +6,6 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing'; 
 
-import {PaginatePipe} from 'ng2-pagination/dist/paginate.pipe';
-import {PaginationControlsComponent} from 'ng2-pagination/dist/pagination-controls.component';
-import {PaginationControlsDirective} from 'ng2-pagination/dist/pagination-controls.directive';
-import {PaginationService} from 'ng2-pagination/dist/pagination.service';
-
 import { NotFoundComponent } from './common/notfound.component';
 
 import {HeaderComponent} from './common/app.header.component';
