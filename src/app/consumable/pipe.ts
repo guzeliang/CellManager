4cf068285f9949779fd7777174330e5a92c46a74
zhanglei <lei.zhang@sunscell.com>
2017-05-12 10:42:12 +0800

    merge

--- src/app/consumable/pipe.ts
+++ src/app/consumable/pipe.ts
@@ -8,16 +8,16 @@ export class MyConsumableType implements PipeTransform {
         let type;
         switch (value) {
             case 1:
-            type = '反应器';
+                type = '反应器';
             break;
-            case '2':
-            type = '清洗机';
+            case 2:
+                type = '清洗机';
             break;
-            case '3':
-            type = '灌流反应器';
+            case 3:
+                type = '灌流反应器';
             break;
             default:
-            type = '通用';
+                type = '通用';
             break;
         }
         return type;
